c5e2b5fd7a749a08f6eed06e9f65604e3089e72c
Rajan Karki <rajankarkee8@gmail.com>
2022-12-23 12:38:47 +0900

    beyond

---
+++ main_project_python/project2.py
@@ -0,0 +1,27 @@
+class dog:
+    
+    
+    
+    # dog attribute
+    
+    attr1 = "mammel"
+    attr2 ="Dog"
+    #return attr1
+    # defining method
+    
+    def my_fun(self):
+        print("I am a", self.attr1)
+                
+        print("I am a", self.attr2)
+        # accessing a value using object of the class..
+sane  = dog()
+
+sane.my_fun()
+
+
+
+
+
+
+
+
