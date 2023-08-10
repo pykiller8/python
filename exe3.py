b0dda031f65a9b0493b05977e14d11dc656b2ab5
pykiller8 <134153265+pykiller8@users.noreply.github.com>
2023-08-10 13:12:42 +0900

    Update exe3.py

--- exe3.py
+++ exe3.py
@@ -1,14 +1,15 @@
 # python module and its features[::]
 
-def greeting(name):  # defining the function with the parameter name "name":
-    print("Hello", +name)
+def greeting(name, age):  # defining the function with the parameter name "name":
+    print("Hello", +name, +age)
     
 person = {            # creating a dictionary with the variable name "person" and assigning the value to it.
     "name": "Rasmika",
-    "Age": 22,
+    "age": 22,
     "address": "Jhapa"   
 }
 print(person) 
 print(person["name"])
+print(person["age"])
 
 # Import the person dictionary from the module[::]
